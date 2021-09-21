1eb9b9bc1b10c2e932124560eeeb1a6aa2b93ed0
vinaydoddapaneni <vinaydoddapaneni@gmail.com>
2021-09-21 23:50:45 +0530

    added the new chatbots which is third party

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
         <Router>
           <Switch>
 
-            <Route to="/" component={Home} />
+            <Route exact path="/" component={Home} />
           </Switch>
         </Router>
       </div>
